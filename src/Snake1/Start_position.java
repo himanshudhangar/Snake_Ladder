bb93858a22d512bfee936a506dd1672fce8d797a
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-03-28 14:37:15 +0530

    first commit

---
+++ src/Snake1/Start_position.java
@@ -0,0 +1,13 @@
+package Snake1;
+import java.util.Scanner;
+public class Start_position {
+
+	public static void main(String[] args) {
+		int playerPosition = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter Player Name: ");
+        String playername = s.next();
+
+	}
+
+}
